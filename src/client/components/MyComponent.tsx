24d6865d1f41a0af7b235f8cd8cce4575791a09a
Dawid Dahl <dawiddahl@gmail.com>
2020-06-24 22:27:26 +0200

    commiting before pushing to heroku

--- src/client/components/MyComponent.tsx
+++ src/client/components/MyComponent.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 export const MyComponent: React.FC = () => {
-	return <h1>Hello from Heroku with React/Typescript!</h1>;
+	return <h1>Hello from Heroku with React/Typescript! Latest Build!</h1>;
 };
 
 export default MyComponent;
