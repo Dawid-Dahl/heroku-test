c10f1bd6b3b67d631dfff21201a886933899fa43
Dawid Dahl <dawiddahl@gmail.com>
2020-06-28 14:47:35 +0200

    switched to build in package.json

--- src/client/components/MyComponent.tsx
+++ src/client/components/MyComponent.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 export const MyComponent: React.FC = () => {
-	return <h1>Hello from Heroku with React/Typescript! New Build!</h1>;
+	return <h1>Hello from Heroku with React/Typescript! Awesome New Build!</h1>;
 };
 
 export default MyComponent;
